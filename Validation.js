ac87ecb92b2808cc17f10bf5417b4bd1a68330e0
Pradeep P <50439015+pradeep1299@users.noreply.github.com>
2020-02-18 23:13:24 +0530

    Add files via upload
    AssPay

--- Validation.js
+++ Validation.js
@@ -52,20 +52,6 @@ function validatemakepayment(){
         document.getElementById("lblName").style.display="none";
         document.getElementById("requiredName").style.display="none";
     }
-    var regex3 = /^(\d{4}|\d{6})$/;
-    if (document.getElementById("txtpincode").value.trim()=="") {
-        document.getElementById("requiredPostcode").style.display="inline";
-        return false;
-    }
-    else if (!document.getElementById("txtpincode").value.match(regex3)) {
-        document.getElementById("lblPostcode").style.display="inline";
-        return false;
-        document.getElementById("requiredPostcode").style.display="none";
-    }
-    else{
-        document.getElementById("lblPostcode").style.display="none";
-        document.getElementById("requiredPostcode").style.display="none";
-    }
     var regex4 = /^[a-zA-Z0-9\s,.'-]{3,}$/;
     if (document.getElementById("txtaddress1").value.trim()=="") {
         document.getElementById("requiredAddress1").style.display="inline";
@@ -73,8 +59,8 @@ function validatemakepayment(){
     }
     else if (!document.getElementById("txtaddress1").value.match(regex4)) {
         document.getElementById("lblAddress1").style.display="inline";
-        return false;
         document.getElementById("requiredAddress1").style.display="none";
+        return false;
     }
 
     else{
@@ -101,42 +87,6 @@ function validatemakepayment(){
     else{
         document.getElementById("lblAddress3").style.display="none";
     }
-    var regex5 = /^[7-9][0-9]{9}$/;
-    if (document.getElementById("txtphone").value.trim()=="") {
-        document.getElementById("lblTelephone").style.display="none";
-    }
-    else if (!document.getElementById("txtphone").value.match(regex5)) {
-        document.getElementById("lblTelephone").style.display="inline";
-        return false;
-    }
-    else{
-        document.getElementById("lblTelephone").style.display="none";
-    }
-    var regex6 = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
-    if (document.getElementById("txtemail").value.trim()=="") {
-        document.getElementById("requiredEmailId").style.display="inline";
-        return false;
-    }
-    else if (!document.getElementById("txtemail").value.match(regex6)) {
-        document.getElementById("lblEmailId").style.display="inline";
-        document.getElementById("requiredEmailId").style.display="none";
-        return false;
-    }
-    else{
-        document.getElementById("lblEmailId").style.display="none";
-        document.getElementById("requiredEmailId").style.display="none";
-    }
-    var regex7 = /[\+? *[1-9]+]?[0-9 ]+/;
-    if (document.getElementById("txtfax").value.trim()=="") {
-       document.getElementById("lblFax").style.display="none";
-    }
-    else if (!document.getElementById("txtfax").value.match(regex7)) {
-       document.getElementById("lblFax").style.display="inline";
-       return false;
-    }
-    else{
-        document.getElementById("lblFax").style.display="none";
-    }
     var regex8 = /^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$/;
     if (document.getElementById("txtcity").value.trim()=="") {
        document.getElementById("requiredCity").style.display="inline";
@@ -160,6 +110,56 @@ function validatemakepayment(){
     }
     else{
         document.getElementById("lblRegion").style.display="none";
+    }
+    var regex3 = /^(\d{4}|\d{6})$/;
+    if (document.getElementById("txtpincode").value.trim()=="") {
+        document.getElementById("requiredPostcode").style.display="inline";
+        return false;
+    }
+    else if (!document.getElementById("txtpincode").value.match(regex3)) {
+        document.getElementById("lblPostcode").style.display="inline";
+        return false;
+        document.getElementById("requiredPostcode").style.display="none";
+    }
+    else{
+        document.getElementById("lblPostcode").style.display="none";
+        document.getElementById("requiredPostcode").style.display="none";
+    }
+    var regex5 = /^[7-9][0-9]{9}$/;
+    if (document.getElementById("txtphone").value.trim()=="") {
+        document.getElementById("lblTelephone").style.display="none";
+    }
+    else if (!document.getElementById("txtphone").value.match(regex5)) {
+        document.getElementById("lblTelephone").style.display="inline";
+        return false;
+    }
+    else{
+        document.getElementById("lblTelephone").style.display="none";
+    }
+    var regex7 = /[\+? *[1-9]+]?[0-9 ]+/;
+    if (document.getElementById("txtfax").value.trim()=="") {
+       document.getElementById("lblFax").style.display="none";
+    }
+    else if (!document.getElementById("txtfax").value.match(regex7)) {
+       document.getElementById("lblFax").style.display="inline";
+       return false;
+    }
+    else{
+        document.getElementById("lblFax").style.display="none";
+    }
+    var regex6 = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
+    if (document.getElementById("txtemail").value.trim()=="") {
+        document.getElementById("requiredEmailId").style.display="inline";
+        return false;
+    }
+    else if (!document.getElementById("txtemail").value.match(regex6)) {
+        document.getElementById("lblEmailId").style.display="inline";
+        document.getElementById("requiredEmailId").style.display="none";
+        return false;
+    }
+    else{
+        document.getElementById("lblEmailId").style.display="none";
+        document.getElementById("requiredEmailId").style.display="none";
     }
 
     return true;
